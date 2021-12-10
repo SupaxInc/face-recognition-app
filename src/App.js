096ab9ce2cc64bcd36de680d00beffc39c92a36c
Paul John Gonzales <paulj_gonzales@hotmail.com>
2021-12-09 23:56:49 -0500

    Changing particles.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import './App.css';
 import Particles from 'react-tsparticles';
+import { ParticlesMain } from "tsparticles";
 import { loadLinksPreset } from "tsparticles-preset-links";
 
 
@@ -61,6 +62,7 @@ class App extends Component {
       }
     }
   }
+
 
   /* Calculates the bounding box inputs */
   calculateFaceBoundingBox = (data) => {
@@ -152,7 +154,7 @@ class App extends Component {
 
     return (
       <div className="App"> 
-        <Particles id="tsparticles" init={this.particlesInit} loaded={this.particlesLoaded}
+        <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded}
               options={particleOptions}
           />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
